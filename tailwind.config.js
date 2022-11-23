ec04802366314aee3be84d00e7625804bda0df45
jnyawach <nyawach41@gmail.com>
2022-11-23 21:25:26 +0300

    todo-initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+     'rubik':['rubik','sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
