7df250bbb4bd9f30ddfc1db0116ab7d9c6d0c5b2
jnyawach <nyawach41@gmail.com>
2022-11-24 06:03:45 +0300

    first-step

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,47 @@
 </script>
 
 <template>
-  <header>
-
-  </header>
-
   <main>
+    <div>
+      <div class="py-5 flex place-content-center header-light">
+        <div class=" w-2/6">
+          <div class="flex justify-between w-full py-14">
+            <h1 class="text-white font-bold text-4xl tracking-widest">TODO</h1>
+            <button type="button">
+              <img :src="'/images/ICON-MOON.SVG'" class="w-6">
+            </button>
 
+          </div>
+          <div class="bg-white flex rounded-lg px-4 py-3 gap-3">
+            <div class="place-content-center self-center">
+              <input type="checkbox" class="self-center checkbox-rounded border m-0 p-0">
+            </div>
+            <div class="place-content-center w-full self-center">
+              <input type="text" placeholder="Create a new to do..." class="self-center text-gray-300 w-full leading-none">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+<style scoped>
+.header-light{
+  background-image: url("/images/bg-desktop-light.jpg");
+  background-position: center;
+  min-height: 280px;
+}
+
+.checkbox-rounded{
+  -moz-appearance:none;
+  -webkit-appearance:none;
+  -o-appearance:none;
+  width: 1.2em;
+  height: 1.2em;
+  border-radius: 50% !important;
+  cursor: pointer;
+  vertical-align: middle;
+}
+</style>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,33 @@ module.exports = {
   ],
   theme: {
     fontFamily:{
-     'rubik':['rubik','sans-serif']
+     'rubik':['rubik','sans-serif'],
+      'josefin':['"Josefin Sans"','sans-serif'],
+    },
+    colors:{
+      transparent:'transparent',
+      'white':'#FFFFFF',
+      'gray':{
+        50:'#fafafa',
+        100:'#e4e5f1',
+        200:'#d2d3db',
+        300:'#9394a5',
+        400:'#484b6a',
+      },
+      'blue':{
+        50:'#cacde8',
+        100:'#e4e5f1',
+        200:'#777a92',
+        300:'#4d5066',
+        400:'#393a4c',
+        500:'#161722',
+        600:'#25273c',
+      },
+      'secondary':{
+        100:'#57ddff',
+        200:'#c058f3'
+      }
+
     },
     extend: {},
   },
